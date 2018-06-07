17a50b6067a3a004a66212713fc821415b54e86a
sinmetal <metal.tie@gmail.com>
2018-06-07 23:36:54 +0900

    add Stackdriver Profiler

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"context"
-	"time"
 	"fmt"
+	"time"
 
+	"cloud.google.com/go/profiler"
 	"github.com/sinmetal/slog"
 )
 
 func main() {
+	if err := profiler.Start(profiler.Config{
+		Service:        "slogtester",
+		ServiceVersion: "1.0.0",
+	}); err != nil {
+		panic(err)
+	}
+
 	errCh := make(chan error)
 
 	go func() {
@@ -38,7 +46,7 @@ func main() {
 		}
 	}()
 
-	<- errCh
+	<-errCh
 }
 
 func workWithCancel(ctx context.Context, v string) {
@@ -48,14 +56,14 @@ func workWithCancel(ctx context.Context, v string) {
 }
 
 func workWithTimeout(ctx context.Context, v string) {
-	cctx, cancel := context.WithTimeout(ctx, 2 * time.Second)
+	cctx, cancel := context.WithTimeout(ctx, 2*time.Second)
 	defer cancel()
 	logger(cctx, fmt.Sprintf("Hello SLOG WithTimeout. %v", v))
 	time.Sleep(3 * time.Second)
 }
 
 func workWithDeadline(ctx context.Context, v string) {
-	cctx, cancel := context.WithDeadline(ctx, time.Now().Add(2 * time.Second))
+	cctx, cancel := context.WithDeadline(ctx, time.Now().Add(2*time.Second))
 	defer cancel()
 	logger(cctx, fmt.Sprintf("Hello SLOG WithDadline. %v", v))
 	time.Sleep(3 * time.Second)
